a6075e9a399d5a4bfda51747c12b2aee57b3e905
Jeffrey Seneff <jeffsnff@gmail.com>
2024-07-23 19:06:42 -0600

    first day of tutorial

---
+++ src/main/GameWindow.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class GameWindow {
+	private JFrame jframe;
+	public GameWindow(GamePanel gamePanel) {
+		// JFrame object
+		jframe = new JFrame();
+		
+		// JFrame window size
+		jframe.setSize(400, 400);
+		// JSFrame close program when window is closed
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(gamePanel);
+		// JFrame window visible
+		jframe.setVisible(true);
+		
+	}
+}
